13b98428860d8d82b7f51a34dc067f7c7a173656
uoshvis <uoshvis@gmail.com>
2017-03-22 11:22:10 +0200

    Update find_duplicate_files.py

--- find_duplicate_files.py
+++ find_duplicate_files.py
@@ -1,17 +1,3 @@
-'''
-Finds duplicate files in given directories.
-Starts by finding files with the same size and computes sha256 of those to find duplicates.
-Thus we can find duplicates with different filename but same content
-Returns duplicate files in duplicates.txt output file
-
-Originally taken from:
-http://pythoncentral.io/finding-duplicate-files-with-python/
-
-Modifications by:
-https://github.com/IanLee1521/utilities/commit/263a0e63503d41df961a352772763e83cf69baf9#diff-880bd89719233299f79774650259c095L11
-http://codereview.stackexchange.com/questions/93546/find-and-remove-duplicate-files-in-one-or-more-directories
-'''
-
 import os
 from sys import argv, exit
 from hashlib import sha256              # collision-resistant and safer than md5
